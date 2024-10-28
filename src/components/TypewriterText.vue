c526354841eda6f86bd6c870e01842025f344c66
Trinethr <72494265+xXTgamerXx@users.noreply.github.com>
2024-10-29 00:21:32 +0530

    added animation

---
+++ src/components/TypewriterText.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const props = defineProps<{
+  words: Array<string>
+}>();
+
+const currentText = ref("");
+
+let i = 0, j=0;
+
+const shuffle = async () => {
+  if (i > 4) i = 0;
+
+  currentText.value = "";
+  let currentWord = props.words[i];
+
+  console.log(currentWord);
+  
+  j=0;
+  await addChar(currentWord);
+
+  i++;
+}
+
+function addChar(text:string) {
+  if (j < text.length) {
+    setTimeout(() => { addChar(text) }, 250)
+    currentText.value += text.charAt(j);
+    j++
+  }
+}
+
+onMounted(() => {
+  shuffle();
+  setInterval(shuffle, 5000);
+})
+</script>
+
+
+<template>
+  <span class="text-accent-500 typewriter-container">
+    <TransitionGroup name="fade" tag="span">
+      <span v-for="(char, index) in currentText" :key="index" class="inline-block">
+        {{ char }}
+      </span>
+    </TransitionGroup>
+  </span>
+</template>
+
+<style>
+
+.fade-enter-active, .fade-leave-active {
+  transition: transform 300ms ease, opacity 300ms ease, font-size 500ms ease;
+}
+
+.fade-enter-from, .fade-leave-to {
+  font-size: 0;
+  opacity: 0;
+}
+
+.fade-enter-to, .fade-leave-from {
+  opacity: 1;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,7 +2,8 @@
 import LoginPageGlow from '@/components/LoginPageGlow.vue';
 import DynamicLogo from '../components/DynamicLogo.vue';
 
-import {ref} from 'vue';
+import { ref } from 'vue';
+import TypewriterText from '@/components/TypewriterText.vue';
 
 const showPassword = ref(false);
 const staySignedIn = ref(true);
@@ -27,24 +28,31 @@ const staySignedIn = ref(true);
       <label>
         <input :type='showPassword ? "text" : "password"' placeholder="⬤⬤⬤⬤⬤⬤⬤⬤">
         <div>Password</div>
-        <button class="showpassword" @click="showPassword = !showPassword"> 
-          <font-awesome-icon :show="showPassword" :icon="showPassword? 'fa-regular fa-eye' : 'fa-regular fa-eye-slash' " />
+        <button class="showpassword" @click="showPassword = !showPassword">
+          <font-awesome-icon :show="showPassword"
+            :icon="showPassword ? 'fa-regular fa-eye' : 'fa-regular fa-eye-slash'" />
         </button>
       </label>
       <label class="text-base mt-1 h-6 flex items-center gap-1.5 w-fit">
         Stay signed in
         <input type="checkbox" class="staySignedIn" v-model="staySignedIn">
       </label>
-      <button class="loginButton p-2 bg-accent-500 bg-opacity-80 rounded-md text-lg mt-2 hover:brightness-110 active:scale-95 transition-[transform,filter]">
+      <button
+        class="loginButton p-2 bg-accent-500 bg-opacity-80 rounded-md text-lg mt-2 hover:brightness-110 active:scale-95 transition-[transform,filter]">
         Login
       </button>
       <div class="divider h-[2px] w-full bg-stone-500 bg-opacity-20 my-3"></div>
-      <div>Forgot Password</div>
-      <div>Don't have an account? Sign Up</div>
+      <div
+        class="hover:text-accent-500 hover:underline cursor-pointer transition-[transform,color] text-center active:scale-95">
+        Forgot Password</div>
+      <div
+        class="hover:text-accent-500 hover:underline cursor-pointer transition-[transform,color] text-center active:scale-95">
+        Don't have an account? Sign Up</div>
     </div>
-    <div class="promo absolute hidden text-5xl leading-snug font-['ClashDisplay-Variable'] xl:flex flex-col left-36 top-1/2 -translate-y-1/2">
+    <div
+      class="promo absolute hidden text-5xl leading-snug font-['ClashDisplay-Variable'] xl:flex flex-col left-36 top-1/2 -translate-y-1/2">
       <h1>A new way to discover</h1>
-      <h1> <span class="text-accent-500">Music</span> with friends.</h1>
+      <h1><TypewriterText :words="['Music', 'Artists', 'Genres', 'Albums', 'Playlists']" /> with friends.</h1>
     </div>
     <LoginPageGlow />
   </div>
@@ -52,21 +60,25 @@ const staySignedIn = ref(true);
 
 <style>
 .navbar {
-  @apply fixed left-0 top-0 w-full h-16 bg-stone-100 dark:bg-black z-50 flex items-center px-8 py-2;
+  @apply fixed left-0 top-0 w-full h-16 bg-stone-100 dark:bg-black z-50 flex items-center justify-center sm:justify-start px-8 py-2;
 }
+
 .left {
   @apply flex items-center;
 }
+
 .appName,
 .appName span {
   font-family: 'ClashDisplay-Variable';
   font-weight: 450;
 }
+
 .login-container {
   @apply select-none p-10 absolute w-full sm:w-96 right-1/2 lg:right-48 top-1/2 flex flex-col gap-1 lg:translate-x-0 translate-x-1/2 -translate-y-1/2 sm:bg-stone-100 sm:dark:bg-stone-950 bg-opacity-30 border-stone-100 sm:border-2 border-opacity-10 rounded-2xl;
 }
 
-input[type="text"], input[type="password"] {
+input[type="text"],
+input[type="password"] {
   @apply text-lg box-border w-full py-2 px-3 bg-stone-100 dark:bg-stone-950 bg-opacity-30 border-stone-100 border-2 border-opacity-10 rounded-md transition-colors;
 }
 
@@ -75,7 +87,8 @@ input::placeholder {
   opacity: 0;
 }
 
-input:focus, input:hover {
+input:focus,
+input:hover {
   @apply outline-none border-accent-500 border-opacity-90;
 }
 
@@ -95,23 +108,27 @@ label div {
   transition: transform 250ms, color 250ms;
 }
 
-label input:focus + div {
+label input:focus+div {
   @apply text-accent-500;
   transform: translate(-0.8rem, -2rem) scale(80%);
 }
-label input:not(:placeholder-shown) + div {
+
+label input:not(:placeholder-shown)+div {
   transform: translate(-0.8rem, -2rem) scale(80%);
 }
 
 .showpassword {
   @apply absolute right-3 top-2 w-6 h-6;
 }
+
 .showpassword svg {
   @apply opacity-40 transition-opacity;
 }
+
 .showpassword svg[show="true"] {
   @apply opacity-90;
 }
+
 .showpassword:hover svg {
   @apply opacity-60;
 }
@@ -134,6 +151,4 @@ input[type="checkbox"]:checked::before {
   @apply scale-100;
 
 }
-
-
 </style>
