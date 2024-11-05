7bff764ac7194123e733a904d4f7a1afae7ace2d
Trinethr N <trinethr@Neels-MacBook-Air.local>
2024-11-06 01:37:54 +0530

    display of error messages in login page, light mode support

--- src/components/TypewriterText.vue
+++ src/components/TypewriterText.vue
@@ -1,45 +1,51 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
 
 const props = defineProps<{
   words: Array<string>
-}>();
+}>()
 
-const currentText = ref("");
+const currentText = ref('')
 
-let i = 0, j=0;
+let i = 0,
+  j = 0
 
 const shuffle = async () => {
-  if (i > 4) i = 0;
+  if (i > 4) i = 0
 
-  currentText.value = "";
-  let currentWord = props.words[i];
-  
-  j=0;
-  await addChar(currentWord);
+  currentText.value = ''
+  const currentWord = props.words[i]
 
-  i++;
+  j = 0
+  addChar(currentWord)
+
+  i++
 }
 
-function addChar(text:string) {
+function addChar(text: string) {
   if (j < text.length) {
-    setTimeout(() => { addChar(text) }, 250)
-    currentText.value += text.charAt(j);
+    setTimeout(() => {
+      addChar(text)
+    }, 250)
+    currentText.value += text.charAt(j)
     j++
   }
 }
 
 onMounted(() => {
-  shuffle();
-  setInterval(shuffle, 5000);
+  shuffle()
+  setInterval(shuffle, 5000)
 })
 </script>
 
-
 <template>
-  <span class="text-accent-500 typewriter-container">
+  <span class="typewriter-container">
     <TransitionGroup name="fade" tag="span">
-      <span v-for="(char, index) in currentText" :key="index" class="inline-block">
+      <span
+        v-for="(char, index) in currentText"
+        :key="index"
+        class="inline-block"
+      >
         {{ char }}
       </span>
     </TransitionGroup>
@@ -47,17 +53,22 @@ onMounted(() => {
 </template>
 
 <style>
-
-.fade-enter-active, .fade-leave-active {
-  transition: transform 300ms ease, opacity 300ms ease, font-size 500ms ease;
+.fade-enter-active,
+.fade-leave-active {
+  transition:
+    transform 300ms ease,
+    opacity 300ms ease,
+    font-size 500ms ease;
 }
 
-.fade-enter-from, .fade-leave-to {
+.fade-enter-from,
+.fade-leave-to {
   font-size: 0;
   opacity: 0;
 }
 
-.fade-enter-to, .fade-leave-from {
+.fade-enter-to,
+.fade-leave-from {
   opacity: 1;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
-import LoginPageGlow from '@/components/LoginPageGlow.vue';
-import DynamicLogo from '@/components/DynamicLogo.vue';
-import TypewriterText from '@/components/TypewriterText.vue';
+import LoginPageGlow from '@/components/LoginPageGlow.vue'
+import DynamicLogo from '@/components/DynamicLogo.vue'
+import TypewriterText from '@/components/TypewriterText.vue'
 
-import { ref } from 'vue';
-import router from '@/router';
+import { ref } from 'vue'
+import router from '@/router'
 
-const authApi = "http://127.0.0.1:5000/auth"
+const authApi = 'http://127.0.0.1:5000/auth'
 
-const showPassword = ref(false);
+const showPassword = ref(false)
 
-const loading = ref(false);
-const email = ref("");
-const password = ref("");
-const staySignedIn = ref(true);
+const loading = ref(false)
+const email = ref('')
+const password = ref('')
+const staySignedIn = ref(true)
+const isError = ref(false)
+const errorText = ref('')
 
 const login = async () => {
-  if (loading.value) return;
-  loading.value = true;
-  let response = await fetch(`${authApi}/login`, {
+  if (loading.value) return
+  loading.value = true
+  isError.value = false
+  const response = await fetch(`${authApi}/login`, {
     method: 'POST',
     credentials: 'include',
     headers: {
@@ -28,16 +31,21 @@ const login = async () => {
       email: email.value,
       password: password.value,
       staySignedIn: staySignedIn.value,
-    })
+    }),
   })
-  let data = await response.json();
-  
-  loading.value = false;
+  const data = await response.json()
+
+  loading.value = false
   if (data.authenticated) {
-    router.push("/");
+    router.push('/')
+  } else {
+    isError.value = true
+    errorText.value = data.message
+    setTimeout(() => {
+      isError.value = false
+    }, 5000)
   }
 }
-
 </script>
 <template>
   <div class="login-page">
@@ -49,44 +57,65 @@ const login = async () => {
         </div>
       </div>
     </div>
-    <div class="login-container">
+    <div :class="'login-container' + (isError ? ' pt-20 p-10' : ' p-10')">
+      <div class="errortext" :iserror="isError" v-html="errorText"></div>
       <h1 class="text-5xl mb-2 font-['ClashDisplay']">Login</h1>
       <label>
-        <input v-model="email" type="text" placeholder="name@example.com">
+        <input v-model="email" type="text" placeholder="name@example.com" />
         <div>Email</div>
       </label>
       <label>
-        <input v-model="password" :type='showPassword ? "text" : "password"' placeholder="⬤⬤⬤⬤⬤⬤⬤⬤">
+        <input
+          v-model="password"
+          :type="showPassword ? 'text' : 'password'"
+          placeholder="●●●●●●●●"
+          @keyup.enter="login()"
+        />
         <div>Password</div>
         <button class="showpassword" @click="showPassword = !showPassword">
-          <font-awesome-icon :show="showPassword"
-            :icon="showPassword ? 'fa-solid fa-eye' : 'fa-solid fa-eye-slash'" />
+          <font-awesome-icon
+            :show="showPassword"
+            :icon="showPassword ? 'fa-solid fa-eye' : 'fa-solid fa-eye-slash'"
+          />
         </button>
       </label>
-      <label class="text-base mt-1 h-6 flex items-center gap-1.5 w-fit">
+      <!--label class="text-base mt-1 h-6 flex items-center gap-1.5 w-fit">
         Stay signed in
-        <input type="checkbox" class="staySignedIn" v-model="staySignedIn">
-      </label>
+        <input type="checkbox" class="staySignedIn" v-model="staySignedIn" />
+      </label -->
+      <div
+        class="hover:text-accent-500 hover:underline cursor-pointer transition-[transform,color] active:scale-95 w-fit"
+      >
+        Forgot Password
+      </div>
       <button
+        type="submit"
         @click="login()"
-        class="loginButton relative p-2 bg-accent-500 bg-opacity-80 rounded-md text-lg mt-2 hover:brightness-110 active:scale-95 transition-[transform,filter]">
+        class="loginButton relative p-2 bg-accent-500 bg-opacity-80 rounded-md text-lg mt-2 hover:brightness-110 active:scale-95 transition-[transform,filter]"
+      >
         Login
-        <div class="loadingSpinner" :class="{'opacity-0': !loading}">
+        <div class="loadingSpinner" :class="{ 'opacity-0': !loading }">
           <font-awesome-icon icon="fa-solid fa-circle-notch" />
         </div>
       </button>
       <div class="divider h-[2px] w-full bg-stone-500 bg-opacity-20 my-3"></div>
       <div
-        class="hover:text-accent-500 hover:underline cursor-pointer transition-[transform,color] text-center active:scale-95">
-        Forgot Password</div>
-      <div
-        class="hover:text-accent-500 hover:underline cursor-pointer transition-[transform,color] text-center active:scale-95">
-        Don't have an account? Sign Up</div>
+        class="hover:text-accent-500 hover:underline cursor-pointer transition-[transform,color] text-center text-lg active:scale-95"
+      >
+        Don't have an account? Sign Up
+      </div>
     </div>
     <div
-      class="promo absolute hidden text-5xl leading-snug font-['ClashDisplay'] xl:flex flex-col left-36 top-1/2 -translate-y-1/2">
+      class="promo absolute hidden text-5xl leading-snug font-['ClashDisplay'] xl:flex flex-col left-36 top-1/2 -translate-y-1/2"
+    >
       <h1>A new way to discover</h1>
-      <h1><TypewriterText :words="['Music', 'Artists', 'Genres', 'Albums', 'Playlists']" /> with friends.</h1>
+      <h1>
+        <TypewriterText
+          class="text-accent-500"
+          :words="['Music', 'Artists', 'Genres', 'Albums', 'Playlists']"
+        />
+        with friends.
+      </h1>
     </div>
     <LoginPageGlow />
   </div>
@@ -108,12 +137,27 @@ const login = async () => {
 }
 
 .login-container {
-  @apply select-none p-10 absolute w-full sm:w-96 right-1/2 xl:right-48 top-1/2 flex flex-col gap-1 xl:translate-x-0 translate-x-1/2 -translate-y-1/2 sm:bg-stone-100 sm:dark:bg-stone-950 bg-opacity-30 sm:dark:bg-opacity-50 border-stone-100 sm:border-2 border-opacity-10 rounded-2xl;
+  @apply overflow-hidden select-none absolute w-full sm:w-96 right-1/2 xl:right-48 top-1/2 flex flex-col gap-1 xl:translate-x-0 translate-x-1/2 -translate-y-1/2 sm:bg-white sm:dark:bg-stone-950 bg-opacity-30 sm:bg-opacity-50 border-stone-600 dark:border-stone-100 sm:border-2 border-opacity-10 dark:border-opacity-10 rounded-2xl;
+  transition: padding 200ms;
 }
 
-input[type="text"],
-input[type="password"] {
-  @apply text-lg box-border w-full py-2 px-3 bg-stone-100 dark:bg-stone-950 bg-opacity-30 border-stone-100 border-2 border-opacity-10 rounded-md transition-colors;
+.errortext {
+  @apply block bg-red-500 bg-opacity-15 absolute top-0 left-0 w-full text-center text-red-600 dark:text-red-400 overflow-hidden box-border h-0 p-0;
+  transition:
+    height 200ms,
+    padding 200ms;
+}
+.errortext[iserror='true'] {
+  @apply h-10 p-2;
+}
+
+a.errorcontact {
+  @apply text-accent-300 underline active:scale-95;
+}
+
+input[type='text'],
+input[type='password'] {
+  @apply text-lg box-border w-full py-2 px-3 bg-stone-100 dark:bg-stone-950 bg-opacity-30 border-stone-600 dark:border-stone-100 border-2 border-opacity-10 dark:border-opacity-10 rounded-md transition-colors;
 }
 
 input::placeholder {
@@ -139,15 +183,17 @@ label div {
   top: 0.5rem;
   left: 0.75rem;
   transform-origin: 0;
-  transition: transform 250ms, color 250ms;
+  transition:
+    transform 250ms,
+    color 250ms;
 }
 
-label input:focus+div {
+label input:focus + div {
   @apply text-accent-500;
   transform: translate(-0.8rem, -2rem) scale(80%);
 }
 
-label input:not(:placeholder-shown)+div {
+label input:not(:placeholder-shown) + div {
   transform: translate(-0.8rem, -2rem) scale(80%);
 }
 
@@ -159,7 +205,7 @@ label input:not(:placeholder-shown)+div {
   @apply opacity-40 transition-opacity;
 }
 
-.showpassword svg[show="true"] {
+.showpassword svg[show='true'] {
   @apply opacity-90;
 }
 
@@ -167,38 +213,24 @@ label input:not(:placeholder-shown)+div {
   @apply opacity-60;
 }
 
-input[type="checkbox"] {
+input[type='checkbox'] {
   @apply cursor-pointer appearance-none flex content-center justify-center text-2xl p-0.5 border-2 border-accent-500 rounded-md transition-colors;
 }
 
-input[type="checkbox"]::before {
-  content: "";
+input[type='checkbox']::before {
+  content: '';
   @apply w-2.5 h-2.5 bg-white dark:bg-stone-950 scale-0 transition-transform;
   clip-path: polygon(15% 50%, 30% 65%, 85% 10%, 100% 25%, 30% 95%, 0% 65%);
 }
 
-input[type="checkbox"]:checked {
+input[type='checkbox']:checked {
   @apply bg-accent-500;
 }
 
-input[type="checkbox"]:checked::before {
+input[type='checkbox']:checked::before {
   @apply scale-100;
-
 }
-@keyframes spinner {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
 .loadingSpinner {
-  position: absolute;
-  top: 0.5rem;
-  right: 0.75rem;
-  animation: spinner 0.75s linear infinite;
-  transition: opacity 200ms;
+  @apply animate-spin absolute top-2 right-3 transition-opacity;
 }
 </style>
