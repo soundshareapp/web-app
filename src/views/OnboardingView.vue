4856f66e94fabbe1496c8e1849b12294bbef686e
Trinethr <72494265+xXTgamerXx@users.noreply.github.com>
2025-01-27 14:58:56 +0530

    getting username and name if already entered

--- src/views/OnboardingView.vue
+++ src/views/OnboardingView.vue
@@ -41,7 +41,8 @@ interface SpotifyUserData {
   uri: string
 }
 
-const spotifyConnected = ref(false)
+const spotifyConnection = ref('')
+const failureReason = ref('')
 const spotifyUserData = ref<SpotifyUserData | null>(null)
 
 const unError = ref(false)
@@ -78,10 +79,24 @@ const logout = async () => {
   }
 }
 
-const nextStep = () => {
-  if (username.value != '' && name.value != '' && !unError.value)
-    currentStep.value = 2
-  else {
+const nextStep = async () => {
+  if (username.value != '' && name.value != '' && !unError.value) {
+    const response = await fetch(`${api}/userdata/update`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username.value,
+        name: name.value,
+      }),
+    })
+    const data = await response.json()
+    if (data.updated.includes('username') && data.updated.includes('name')) {
+      currentStep.value = 2
+    }
+  } else {
     inputShake.value[0] = !usernameRegex.test(username.value)
     inputShake.value[1] = name.value == ''
     setTimeout(() => (inputShake.value[0] = false), 350)
@@ -107,7 +122,20 @@ const spotifyLogin = async () => {
   window.location.href = data.url
 }
 
-const get_spotify_data = async () => {
+const getUserData = async () => {
+  await fetch(`${api}/userdata/username`, { credentials: 'include' }).then(
+    async data => {
+      username.value = await data.text()
+    },
+  )
+  await fetch(`${api}/userdata/name`, { credentials: 'include' }).then(
+    async data => {
+      name.value = await data.text()
+    },
+  )
+}
+
+const getSpotifyData = async () => {
   const response = await fetch(`${api}/spotify/user-info`, {
     credentials: 'include',
   })
@@ -115,11 +143,16 @@ const get_spotify_data = async () => {
 }
 
 onMounted(() => {
-  spotifyConnected.value =
-    Boolean(router.currentRoute.value.query.spotify_connected) || false
-  if (spotifyConnected.value) {
+  getUserData()
+  spotifyConnection.value = String(
+    router.currentRoute.value.query.spotify_connection,
+  )
+  if (spotifyConnection.value == 'success') {
     currentStep.value = 2
-    get_spotify_data()
+    getSpotifyData()
+  } else if (spotifyConnection.value == 'failed') {
+    currentStep.value = 2
+    failureReason.value = String(router.currentRoute.value.query.reason)
   }
 })
 </script>
@@ -138,13 +171,8 @@ onMounted(() => {
         <div class="onboarding-step">
           <h1 class="text-2xl font-medium">Let's start with the basics</h1>
           <label>
-            <input
-              v-model="username"
-              type="text"
-              placeholder="curiouspineapple"
-              @input="testUsername"
-              :class="(unError ? 'err ' : '') + (inputShake[0] ? 'shake' : '')"
-            />
+            <input v-model="username" type="text" placeholder="curiouspineapple" @input="testUsername"
+              :class="(unError ? 'err ' : '') + (inputShake[0] ? 'shake' : '')" />
             <div>Pick a username</div>
             <p class="text-sm">
               5-20 characters, can consist of a-z, 0-9, period, hyphen and
@@ -152,13 +180,8 @@ onMounted(() => {
             </p>
           </label>
           <label>
-            <input
-              v-model="name"
-              type="text"
-              placeholder="John Doe"
-              :class="inputShake[1] ? 'shake' : ''"
-              @keyup.enter="nextStep()"
-            />
+            <input v-model="name" type="text" placeholder="John Doe" :class="inputShake[1] ? 'shake' : ''"
+              @keyup.enter="nextStep()" />
             <div>Choose your display name</div>
             <p class="text-sm">Can be your real name or nickname.</p>
           </label>
@@ -168,53 +191,49 @@ onMounted(() => {
         </div>
         <div class="onboarding-step">
           <h1 class="text-2xl font-medium">Link your streaming service</h1>
-          <button
-            class="linkbutton transition-colors p-3 my-2 text-white text-base rounded-xl relative"
-            @click="spotifyLogin"
-          >
-            <img
-              class="w-7 h-7 left-5 top-2.5 absolute"
-              src="../assets/spotify.svg"
-            />
+          <button class="linkbutton transition-colors p-3 my-2 text-white text-base rounded-xl relative"
+            @click="spotifyLogin">
+            <img class="w-7 h-7 left-5 top-2.5 absolute" src="../assets/spotify.svg" />
             <div class="translate-x-[3%] font-bold">CONNECT WITH SPOTIFY</div>
           </button>
-          <div v-if="!spotifyConnected">Not connected</div>
-          <p v-if="spotifyConnected" class="font-medium">Your profile</p>
-          <div
-            v-if="spotifyConnected"
-            class="spotifyUserCard"
-            :style="{
-              'background-image': `url(${spotifyUserData?.images?.[0]?.url}`,
-            }"
-          >
-            <img
-              :src="spotifyUserData?.images?.[0]?.url"
-              class="w-16 h-16 rounded-md"
-            />
+          <p v-if="spotifyConnection == 'success'" class="font-medium">
+            Your Spotify profile
+          </p>
+          <div v-if="spotifyConnection === 'success'" class="spotifyUserCard" :style="{
+            'background-image': `url(${spotifyUserData?.images?.[0]?.url}`,
+          }">
+            <img :src="spotifyUserData?.images?.[0]?.url" class="w-16 h-16 rounded-md" />
             <div class="flex flex-col">
               <div class="text-xl font-medium">
                 {{ spotifyUserData?.display_name }}
                 <p class="inline text-lg">
                   {{ countryCodeToEmoji(spotifyUserData?.country || '') }}
                 </p>
-                <font-awesome-icon
-                  icon="dollar-sign"
-                  title="Premium"
-                  class="bg-gradient-to-tr from-[#1ed760d0] to-[#1ed780d0] ml-1 p-1 w-3 h-3 rounded-full"
-                />
+                <font-awesome-icon icon="dollar-sign" title="Premium"
+                  class="bg-gradient-to-tr from-[#1ed760d0] to-[#1ed780d0] ml-1 p-1 w-3 h-3 rounded-full" />
               </div>
               <div class="opacity-80 text-sm">
                 Followers: {{ spotifyUserData?.followers.total }}
               </div>
-              <div class="opacity-70 text-xs">{{ spotifyUserData?.email.replace(emailCensorRegex, (x,y) => y || '*') }}</div>
+              <div class="opacity-70 text-xs">
+                {{
+                  spotifyUserData?.email.replace(
+                    emailCensorRegex,
+                    (_, y) => y || '*',
+                  )
+                }}
+              </div>
             </div>
-            <div
-              class="cardBlur"
-            ></div>
+            <div class="cardBlur"></div>
             <a class="userlink" :href="spotifyUserData?.external_urls.spotify" target="_blank">
               <font-awesome-icon icon="arrow-up-right-from-square" />
-            </a >
+            </a>
           </div>
+          <div v-else-if="spotifyConnection === 'failed'">
+            <p class="text-lg text-red-500">Connection to Spotify failed</p>
+            <p>Reason: {{ failureReason }}</p>
+          </div>
+          <div v-else>Not connected</div>
           <div class="flex-grow" />
           <button class="prevbutton" @click="currentStep = 1">
             <font-awesome-icon icon="arrow-left" />
@@ -224,8 +243,7 @@ onMounted(() => {
     </div>
     <button
       class="bg-stone-900 dark:bg-stone-100 bg-opacity-10 dark:bg-opacity-10 py-2 px-4 my-4 mx-auto rounded-md absolute bottom-0"
-      @click="logout"
-    >
+      @click="logout">
       Logout (Complete Later)
     </button>
     <PageGlow />
@@ -259,7 +277,7 @@ input.shake {
   animation: inputShake ease-out 0.35s;
 }
 
-label input.err + div {
+label input.err+div {
   @apply text-red-600 dark:text-red-500;
 }
 
@@ -292,7 +310,9 @@ label input.err + div {
 }
 
 .userlink {
-  @apply absolute top-0 right-0 p-2 h-8 w-8 cursor-pointer flex bg-white bg-opacity-5 hover:bg-opacity-10 active:bg-opacity-15 transition-colors text-white rounded-tr-xl rounded-bl-lg;}
+  @apply absolute top-0 right-0 p-2 h-8 w-8 cursor-pointer flex bg-white bg-opacity-5 hover:bg-opacity-10 active:bg-opacity-15 transition-colors text-white rounded-tr-xl rounded-bl-lg;
+}
+
 .userlink svg {
   @apply w-full h-full;
 }
@@ -308,7 +328,7 @@ label input.err + div {
 
   50% {
     transform: translateX(-0.3rem);
-    @apply brightness-90 dark:brightness-150;
+    @apply bg-stone-500 bg-opacity-15;
   }
 
   75% {
